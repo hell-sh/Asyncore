34425e494139a89e2297f17f30b8c487700283e2
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-11-30 20:47:57 +0100

    Update docs

--- docs/search/functions_6.js
+++ docs/search/functions_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['init_37',['init',['../classpas_1_1pas.html#ae50f4a14fddf333454118abcf1345014',1,'pas\pas\init()'],['../classpas_1_1stdin.html#a8a85c928a26b0bf780133f85741cce9b',1,'pas\stdin\init()']]],
+  ['init_37',['init',['../classpas_1_1pas.html#ae50f4a14fddf333454118abcf1345014',1,'pas\pas\init()'],['../classpas_1_1stdin.html#a645a60e1743a2bd7e42cd2f0334e7fed',1,'pas\stdin\init()']]],
   ['istrue_38',['isTrue',['../classpas_1_1AlwaysTrueCondition.html#a8459268c8aa8a8dde6aa456891515244',1,'pas\AlwaysTrueCondition\isTrue()'],['../classpas_1_1Condition.html#a7367305693b521aa1ebf2b87535fa50e',1,'pas\Condition\isTrue()']]],
   ['iswindows_39',['isWindows',['../classpas_1_1pas.html#aa0f421640d2aa73166790024453231ed',1,'pas::pas']]]
 ];
